2e1f8340a70badbad0f95b903906e9b9181535f0
Brajkishore-yadav <braj.2024ug2027@iiitranchi.ac.in>
2026-09-01 11:18:18 +0530

    Time: 0 ms (100%), Space: 10 MB (70.71%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,31 +1,51 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        vector<int>s1hash(26,0);
-        vector<int>s2hash(26,0);
+
+        vector<int> s1hash(26, 0);
+        vector<int> s2hash(26, 0);
+
         int n1 = s1.size();
         int n2 = s2.size();
-        if(n1>n2){
+
+        if(n1 > n2) {
             return false;
         }
-        int left=0,right=0;
-        while(right<n1){
-            s1hash[s1[right]-'a']++;
-            s2hash[s2[right]-'a']++;
+
+        int left = 0;
+        int right = 0;
+
+        // First window
+        while(right < n1) {
+
+            s1hash[s1[right] - 'a']++;
+            s2hash[s2[right] - 'a']++;
+
             right++;
         }
-        right--;
-        while(right<n2){
-            if(s1hash==s2hash){
+
+        // Sliding window
+        while(right < n2) {
+
+            if(s1hash == s2hash) {
                 return true;
             }
-            right++;
-            if(right!=n2){
-                s2hash[s2[right]-'a']++;
-            }
-            s2hash[s2[left]-'a']--;
+
+            // Add new character
+            s2hash[s2[right] - 'a']++;
+
+            // Remove old character
+            s2hash[s2[left] - 'a']--;
+
             left++;
+            right++;
         }
+
+        // Check the last window
+        if(s1hash == s2hash) {
+            return true;
+        }
+
         return false;
     }
 };

9f8b88fda12973713f9b042b048a674ac563d814
Brajkishore-yadav <braj.2024ug2027@iiitranchi.ac.in>
2026-09-12 12:43:59 +0530

    Time: 0 ms (100%), Space: 15.3 MB (37.32%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            // Target mil gaya
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Left half sorted hai
+            if (nums[left] <= nums[mid]) {
+
+                // Target left sorted half mein hai
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                }
+                else {
+                    // Target left mein nahi hai, right mein jao
+                    left = mid + 1;
+                }
+            }
+
+            // Right half sorted hai
+            else {
+
+                // Target right sorted half mein hai
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                }
+                else {
+                    // Target right mein nahi hai, left mein jao
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};

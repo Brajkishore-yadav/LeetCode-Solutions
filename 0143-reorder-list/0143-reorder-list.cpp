1871ed2de511fa6d2edf9cd40076613d5fcc0ca6
Brajkishore-yadav <braj.2024ug2027@iiitranchi.ac.in>
2026-09-19 16:43:24 +0530

    Time: 0 ms (100%), Space: 22.9 MB (51.77%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -1,43 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        if (head == NULL || head->next == NULL)
+        if (!head || !head->next) {
             return;
+        }
+        // get two lists
+        ListNode* fast = head;
+        ListNode* slow = head;
+        ListNode* prev = nullptr;
+        while (fast != nullptr && fast->next) {
+            fast = fast->next->next;
+            prev = slow;
+            slow = slow->next;
+        }
+        prev->next = nullptr;
 
-        vector<ListNode*> nodes;
+        // now we have two lists, prev and head
 
-        ListNode* temp = head;
-
-        // Saare nodes store karo
-        while (temp != NULL) {
-            nodes.push_back(temp);
-            temp = temp->next;
+        // let's reverse the second
+        ListNode* node = slow;
+        ListNode* prev2 = nullptr;
+        while (node != nullptr) {
+            ListNode* next = node->next;
+            node->next = prev2;
+            prev2 = node;
+            node = next;
         }
 
-        int left = 0;
-        int right = nodes.size() - 1;
+        // let's merge them
+        ListNode* first = head;
+        ListNode* second = prev2;
+        while (second) {
+            ListNode* tmp1 = first->next;
+            ListNode* tmp2 = second->next;
 
-        // Teri approach:
-        // left -> left side se
-        // right -> last side se
-        while (left < right) {
-
-            // left ke baad right lagao
-            nodes[left]->next = nodes[right];
-
-            left++;
-
-            // Agar cross ho gaye
-            if (left == right)
+            first->next = second;
+            if (!tmp1) {
                 break;
+            }
+            second->next = tmp1;
+           
 
-            // right ke baad next left lagao
-            nodes[right]->next = nodes[left];
-
-            right--;
+            first = tmp1;
+            second = tmp2;
         }
 
-        // Last node ka next NULL
-        nodes[left]->next = NULL;
     }
 };
